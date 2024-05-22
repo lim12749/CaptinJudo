4dae01f8f626959af383ba196b46951d8391d693
lim12749 <50003828+lim12749@users.noreply.github.com>
2024-05-23 03:03:58 +0900

    css파일 추가하고싶어..

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import ImageSlider from "@/components/index";
+import CardBackGroundComponent from "@/components/Cardbackground";
 
 
 export default function Home() {
   return (
-      <main className="sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-purple-100
-     2xl:bg-orange-100 min-h-screen flex items-center justify-center ">
+      <main className=" min-h-screen flex items-center justify-center ">
         <header className="w-full fixed top-0 z-50 bg-gray-800  text-white py-4 flex justify-between items-center">
-          <p className="text-2xl font-bold ml-4"> CPT 캡틴 유도관</p>
+          <p className="text-xl font-bold ml-2"> CPT 캡틴 유도관</p>
           <nav>
-            <ul className="flex space-x-4 mr-4">
-              <li><p className="text-xs">캡틴 소개</p></li>
+            <ul className="flex space-x-4 mr-2">
+              <li><p className="text-xs">소개</p></li>
               <li><p className="text-xs">프로필</p></li>
               <li><p className="text-xs">목표</p></li>
               <li><p className="text-xs">시설 사진</p></li>
@@ -19,35 +19,26 @@ export default function Home() {
           </nav>
         </header>
 
-        <div className="flex flex-col w-full">
-          <div className="bg-white h-screen flex items-center justify-center">
-            <div className="text-blue-600 text-2xl">
-              <p>캡틴 유도관 </p>
-              <p> 운동 프로그램</p>
+        <div className="flex flex-col w-full relative ">
+          <div className="bg-white h-screen flex items-center justify-center relative">
+            <div className="">
+              <p className="px-4 mt-[-10rem] font-bold text-9xl">CAPTAIN JUDO </p>
+              <p className="text-right font-bold font text-2xl mr-14"> 유능제강 </p>
+              <p className="text-right font-bold text-2xl  mt-4 mr-14"> 예시예종 </p>
+            </div>
+            <div className="flex space-x-3 absolute bottom-4 right-4">
+              <button className="bg-yellow-300 rounded-xl w-12 h-12"></button>
+              <button className="bg-purple-500 rounded-xl w-12 h-12"></button>
             </div>
           </div>
-          <div className="bg-gray-200 h-screen flex items-center justify-center">
-            <div className="flex flex-col items-center p-5">
-              <div className="
-              bg-white text-black rounded-2xl w-full shadow-lg p-5 ">관장님 소개
-                <Image src="/img/Profile.jpg" alt="test" width={300} height={300}/>
-                <p className="text-xs font-medium py-5">[용. 인. 시. 최. 초. 부. 부. 유. 도. 장]
 
-                  용인대 유도학과 출신의 젊은 부부가 함께 운영하는 유도장입니다.
-                  두 아이의 부모로서 항상 부모의 마음으로 지도하고 있습니다.
-
-                  여자 관장님의 지도 아래 유도를 배우고 싶은 여성분들이 있다면 꼭 연락 주세요!!!
-                  (여자 관장님이 직접 문의를 받고 있으니 부담 없이 문의하세요!!)
-
-                  707특수임무단 예비역 대위(ROTC 55기) 출신 관장님의 체계적인 운동 프로그램으로 유도뿐만 아니라 체력증진 및 다이어트가 가능합니다.
-
-                  언제든 문의하시면 친절하게 상담 도와드리겠습니다.
-
-                  즐겁고 건강한 유도 함께해요!
-
-                  카톡 채널 - 용인대 캡틴 유도</p>
-              </div>
-
+          <div className="bg-red-200 h-screen flex items-center justify-center m-4">
+            <div className="
+              bg-white text-black rounded-2xl w-full shadow-lg p-5 flex-col inline-flex ">관장님 소개
+              <Image src="/img/Profile.jpg" alt="test" width={200} height={200}/>
+              <p className="text-xs font-medium py-5">
+                [용. 인. 시. 최. 초. 부. 부. 유. 도. 장]
+              </p>
             </div>
           </div>
           <div className="bg-white h-screen flex items-center justify-center">
@@ -64,6 +55,12 @@ export default function Home() {
           <div className="bg-white h-screen flex items-center justify-center">
             <div className="text-orange-500 text-2xl"> 오시는길
             </div>
+          </div>
+          <div className="w-full fixed bottom-0 bg-gray-700 flex justify-center">
+            <p className="text-xl font-bold ml-2 text-white">footer</p>
+          </div>
+          <div className="w-full fixed bottom-0 bg-gray-700 flex justify-center">
+            <p className="text-xl font-bold ml-2 text-white">footer</p>
           </div>
         </div>
       </main>
---
+++ components/Cardbackground.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Image from "next/image";
+
+export default function CardBackGroundComponent()
+{
+    return (
+        <div className=" bg-white text-black rounded-2xl w-full shadow-lg p-5 flex-col inline-flex ">
+        </div>
+    );
+}
